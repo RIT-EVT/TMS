63657080c464e3fb9031089ccadcb870fa2db54c
cbolles <collinbolles@gmail.com>
2022-04-06 19:30:19 -0400

    Change testing to add delay before reading

--- targets/muxThermistorSample/main.cpp
+++ targets/muxThermistorSample/main.cpp
@@ -6,20 +6,15 @@
 #include <EVT/io/pin.hpp>
 #include <EVT/utils/time.hpp>
 #include <TMS/TMS.hpp>
+#include <EVT/io/ADC.hpp>
 #include <cstdlib>
+#include <EVT/utils/time.hpp>
 
 namespace IO = EVT::core::IO;
 namespace time = EVT::core::time;
 
 static uint32_t conversion(uint32_t adcInput) {
-    auto adc64 = (uint64_t) adcInput;
-    auto outTemp = (uint32_t) ((adc64 * adc64 * adc64) / 1000000000 * 9604
-                               - (adc64 * adc64) / 100000 * 4502
-                               + adc64 * 9774 / 100
-                               - 7374);
-    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "  -  Temp: %08x", outTemp);
-
-    return outTemp;
+    return adcInput;
 }
 
 int main() {
@@ -27,15 +22,13 @@ int main() {
 
    char buf[100];
 
-   IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+   IO::UART& uart = IO::getUART<IO::Pin::PB_10, IO::Pin::PB_11>(9600);
 
    IO::GPIO* mux1 = &IO::getGPIO<IO::Pin::PB_2>();
    IO::GPIO* mux2 = &IO::getGPIO<IO::Pin::PB_8>();
-   IO::GPIO* mux3 = &IO::getGPIO<IO::Pin::PB_9>();
 
 
-   // Set up TMS and necessary device drivers
-   DEV::Thermistor therm = DEV::Thermistor(IO::getADC<IO::Pin::PA_4>(), conversion);
+   IO::ADC& adc = IO::getADC<IO::Pin::PA_4>();
 
    uint8_t thermNumber = 0;
    while (1) {
@@ -49,17 +42,14 @@ int main() {
        } else {
            mux2->writePin(IO::GPIO::State::LOW);
        }
-       if (thermNumber & 4U) {
-           mux3->writePin(IO::GPIO::State::HIGH);
-       } else {
-           mux3->writePin(IO::GPIO::State::LOW);
-       }
 
-       uart.printf("Temp value: %d", therm.getTempCelcius());
+       time::wait(500);
+       uart.printf("Temp value for %d: %dmv\n\r", thermNumber,
+                                                  (int)(adc.read() * 1000));
 
-       uart.printf("Press enter to move to the next thermistor.");
+       uart.printf("Press enter to move to the next thermistor.\n\r");
        uart.gets(buf, 100);
        uart.printf("\n\r");
-       thermNumber = (thermNumber + 1) % 8;
+       thermNumber = (thermNumber + 1) % 4;
    }
 }
