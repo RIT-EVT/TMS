4fa5338021b87c9702a9da545cd16213db4be0ce
GitHub Build <N/A>
2022-10-21 15:05:22 +0000

    Applied Formatting Changes During GitHub Build

--- targets/adcSample/main.cpp
+++ targets/adcSample/main.cpp
@@ -1,9 +1,9 @@
 /**
  * Tests the values produced by the ADC along with a rolling average voltage
  */
+#include <EVT/io/ADC.hpp>
 #include <EVT/io/GPIO.hpp>
 #include <EVT/io/manager.hpp>
-#include <EVT/io/ADC.hpp>
 #include <EVT/utils/time.hpp>
 
 namespace IO = EVT::core::IO;
@@ -12,13 +12,13 @@ namespace time = EVT::core::time;
 
 uint32_t calcAvg(uint32_t lastTen[]) {
     uint32_t avg = 0;
-    for(int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
         avg += lastTen[i];
     }
     return avg / 10;
 }
 
-void switchMux(IO::GPIO& muxs3, IO::GPIO& muxs2, IO::GPIO& muxs1, uint8_t s3, uint8_t s2, uint8_t s1){
+void switchMux(IO::GPIO& muxs3, IO::GPIO& muxs2, IO::GPIO& muxs1, uint8_t s3, uint8_t s2, uint8_t s1) {
     s3 == 0 ? muxs3.writePin(IO::GPIO::State::LOW) : muxs3.writePin(IO::GPIO::State::HIGH);
     s2 == 0 ? muxs2.writePin(IO::GPIO::State::LOW) : muxs2.writePin(IO::GPIO::State::HIGH);
     s1 == 0 ? muxs1.writePin(IO::GPIO::State::LOW) : muxs1.writePin(IO::GPIO::State::HIGH);
@@ -35,7 +35,7 @@ int main() {
     IO::GPIO& muxs3 = IO::getGPIO<IO::Pin::PB_9>();
 
     uint32_t rollAvg = 0;
-    uint32_t lastTen[] = {0,0,0,0,0,0,0,0,0,0};
+    uint32_t lastTen[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     unsigned int counter = 0;
     unsigned int looper = 1;
     uint32_t currV1 = -1;
@@ -47,7 +47,7 @@ int main() {
 
     time::wait(500);
 
-    while(1){
+    while (1) {
         /**
          * The discrepency with the case and voltage index is due to an apparent miswiring in the TMS itself
          * The conversion to read temperatures in the correct order is as follows:
@@ -56,9 +56,8 @@ int main() {
          * [Temp 3] is actually from [Temp 4]
          * [Temp 4] is actually from [Temp 1]
          */
-        for(int i = 0; i < 100; i++) {
-            switch (looper)
-            {
+        for (int i = 0; i < 100; i++) {
+            switch (looper) {
             case 1:
                 switchMux(muxs3, muxs2, muxs1, 0, 0, 0);
                 time::wait(1);
@@ -103,7 +102,7 @@ int main() {
             uart.printf("%d, %d, %d, %d, ", currV1, currV2, currV3, currV4);
             uart.printf("%d\r\n", rollAvg);
 
-            if(counter == 10) {
+            if (counter == 10) {
                 counter = 0;
             } else {
                 counter++;
