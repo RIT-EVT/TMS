95486f73160a93bd055e6201a1ed6c659c428e19
Aaron Chan <aarchan108@gmail.com>
2023-02-16 16:03:59 -0500

    TCA Header docs

--- include/TMS/dev/TCA9545A.h
+++ include/TMS/dev/TCA9545A.h
@@ -9,6 +9,9 @@ namespace TCA9545A {
 
 constexpr uint8_t TCA9545A_I2C_ADDR = 0x73; // Address can change based on pins
 
+/**
+ * Represents the registers for setting the active bus on the TCA9545A
+ */
 enum TCA9545A_BUS {
     BUS_0 = 0x01,
     BUS_1 = 0x02,
@@ -16,10 +19,20 @@ enum TCA9545A_BUS {
     BUS_3 = 0x08,
 };
 
+/**
+ * TCA9545A I2C Multiplexer Driver
+ * https://www.ti.com/lit/ds/symlink/tca9545a.pdf?ts=1676581408943
+ */
 class TCA9545A {
 public:
     TCA9545A(IO::I2C& i2c);
 
+    /**
+     * Sets the active bus on the TCA9545A
+     * @param bus[in] The bus to set active
+     * @param toggled[in] Whether to toggle the bus or not
+     * @return Result of the I2C write operation
+     */
     IO::I2C::I2CStatus setBus(uint8_t bus, bool toggled);
 
     IO::I2C::I2CStatus toggleBus(uint8_t bus);
@@ -27,8 +40,21 @@ public:
 private:
     IO::I2C& i2c;
 
+    /**
+     * Writes a value to a register on the TCA9545A
+     * @param reg[in] The register to write to
+     * @param val[in] The value to write to the register
+     * @return Result of the I2C write operation
+     */
     IO::I2C::I2CStatus writeRegister(uint8_t reg, uint8_t val);
 
+    /**
+     * Reads a value from a register on the TCA9545A
+     * @param reg[in] The register to read from
+     * @param val[out] The value read from the register
+     * @param len[in] The length of the value to read
+     * @return Result of the I2C read operation
+     */
     IO::I2C::I2CStatus readRegister();
 };
 }
