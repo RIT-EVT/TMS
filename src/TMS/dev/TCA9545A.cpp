a2e9a5d83699ec7eea2307bfde062e07d2738696
Aaron Chan <aarchan108@gmail.com>
2023-02-20 19:42:17 -0500

    Replace breaks with returns

--- src/TMS/dev/TCA9545A.cpp
+++ src/TMS/dev/TCA9545A.cpp
@@ -9,25 +9,17 @@ IO::I2C::I2CStatus TCA9545A::setBus(uint8_t bus, bool toggled) {
     uint8_t val = toggled ? 1 : 0;
     switch (bus) {
     case 0:
-        ret = writeRegister(TCA9545A_BUS::BUS_0, val);
-        break;
-
+        return writeRegister(TCA9545A_BUS::BUS_0, val);
     case 1:
-        ret = writeRegister(TCA9545A_BUS::BUS_1, val);
-        break;
-
+        return writeRegister(TCA9545A_BUS::BUS_1, val);
     case 2:
-        ret = writeRegister(TCA9545A_BUS::BUS_2, val);
-        break;
-
+        return writeRegister(TCA9545A_BUS::BUS_2, val);
     case 3:
-        ret = writeRegister(TCA9545A_BUS::BUS_3, val);
-        break;
+        return writeRegister(TCA9545A_BUS::BUS_3, val);
     default:
         return IO::I2C::I2CStatus::ERROR;
     }
 
-    return ret;
 }
 
 IO::I2C::I2CStatus TCA9545A::writeRegister(uint8_t reg, uint8_t val) {
