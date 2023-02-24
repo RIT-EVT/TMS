8d2de9a97e4d8a1f3f74bbbbc8c7cc4f1435e80a
Ishan Renjen <66697119+ishan-renjen@users.noreply.github.com>
2023-02-23 20:08:53 -0500

    matt made me do more comments

--- include/TMS/dev/TMP117.hpp
+++ include/TMS/dev/TMP117.hpp
@@ -14,20 +14,33 @@ namespace TMS {
  */
 class TMP117 {
 public:
-    /**Temp sensor constructor
+    /**
+     * Temp sensor constructor
+     *
      * @param i2c used to read temperature
      * @param deviceID - device address
      * */
     TMP117(IO::I2C& i2c, uint8_t deviceID);
-    /**reads the temperature*/
+
+    /**
+     * Reads the temperature
+     *
+     * @return
+     */
     uint16_t readTemp();
 
 private:
+    /**register for temperature values */
     uint8_t tempReg;
+    /**device ID*/
     uint8_t deviceID;
+    /**i2c declaration*/
     IO::I2C& i2c;
+    /**length of the temperature register in bytes*/
     uint8_t regLength = 1;
+    /**length of the value being read in bytes*/
     uint8_t valueLength = 2;
+    /**16-bit temp value returned from readTemp() in degrees centi celsius*/
     uint16_t temp;
 };
 }
