8dcff41d4e294ca8aa1193db367c137bda9dd084
GitHub Build <N/A>
2023-02-28 00:39:35 +0000

    Applied Formatting Changes During GitHub Build

--- src/TMS/dev/TMP117.cpp
+++ src/TMS/dev/TMP117.cpp
@@ -1,5 +1,5 @@
-#include <TMS/dev/TMP117.hpp>
 #include <EVT/io/I2C.hpp>
+#include <TMS/dev/TMP117.hpp>
 
 namespace TMS {
 
@@ -13,16 +13,16 @@ uint16_t TMP117::readTemp() {
 
     i2c.readReg(deviceID, &tempReg, regLength, tempBytes, valueLength);
 
-    input = ((uint16_t) tempBytes[0]) << 8|tempBytes[1];
+    input = ((uint16_t) tempBytes[0]) << 8 | tempBytes[1];
 
     /**
      * degrees centi celsius
      * multiplied by 78125 because the sensor output increases by .0078125 degrees celsius - brings it to fixed point within 64 bits
      * divided by 100000 so it fits in a 16 bit int
      */
-    temp = ((uint64_t)input)*78125/100000;
+    temp = ((uint64_t) input) * 78125 / 100000;
 
     return temp;
 }
 
-}
+}// namespace TMS
